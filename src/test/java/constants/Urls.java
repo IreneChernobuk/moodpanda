6fe350af10468ead018949fb508a43a22cc163ad
IreneChernobuk <irinka_buc@mail.ru>
2023-03-26 22:38:41 +0300

    TMS test with cucumber but with pages

--- src/test/java/constants/Urls.java
+++ src/test/java/constants/Urls.java
@@ -2,4 +2,5 @@ package constants;
 
 public class Urls {
     public static String LOGIN_URL = "https://moodpanda.com/login";
+    public static final String TMS_MAIN_URL = "https://teachmeskills.by";
 }
--- src/test/java/steps/SearchSteps.java
+++ src/test/java/steps/SearchSteps.java
@@ -1,20 +1,22 @@
 package steps;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.testng.Assert;
+import pages.tmspages.CourseTypePage;
+import pages.tmspages.CoursesListPage;
+import pages.tmspages.MainPage;
 
 import java.awt.*;
 
-import static com.codeborne.selenide.Selenide.*;
-
 public class SearchSteps {
+    MainPage mainPage;
+    CourseTypePage courseTypePage;
+    CoursesListPage coursesListPage;
     @Before
     public void setUp() {
         Configuration.timeout = 6000;
@@ -22,30 +24,29 @@ public class SearchSteps {
         int width = (int) size.getWidth();
         int height = (int) size.getHeight() - 50;
         Configuration.browserSize = String.format("%dx%d", width, height);
+        mainPage = new MainPage();
+        courseTypePage = new CourseTypePage();
+        coursesListPage = new CoursesListPage();
     }
 
     @Given("Main TMS page opened")
     public void mainTMSPageOpened() {
-        open("https://teachmeskills.by");
+        mainPage.openMainPage();
     }
 
     @When("User goes to {string}")
     public void userGoesTo(String tabName) {
-        String tabXpath = String.format("//div[contains(@class, 'centerside')]//a[text()='%s']", tabName);
-        $(By.xpath(tabXpath)).shouldBe(Condition.visible).click();
+        mainPage.clickNavigationMenu(tabName);
     }
 
     @And("Selects {string}")
     public void selects(String courseType) {
-        String courseTypeXpath = String.format("//div[text()='%s']", courseType);
-        $(By.xpath(courseTypeXpath)).shouldBe(Condition.visible).hover();
-        $(By.xpath("//div[text()='Перейти'][1]")).click();
+        courseTypePage.selectCourseType(courseType);
     }
 
     @Then("{string} course presents on page")
     public void coursePresentsOnPage(String courseName) {
-        String courseNameXpath = String.format("//strong[text()='%s']", courseName);
-        int quantityOfCourses = $$(By.xpath(courseNameXpath)).size();
-        Assert.assertEquals(quantityOfCourses, 2, "Quantity of courses !=2");
+        int quantityOfCourses = coursesListPage.getQuantityOfCourses(courseName);
+              Assert.assertEquals(quantityOfCourses, 2, "Quantity of courses !=2");
     }
 }
